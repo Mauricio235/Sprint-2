593ecc2f730e704ec38cdda2586df2718a2fd36e
Mauricio Anjos <mauriciow625@gmail.com>
2022-02-05 09:13:11 -0300

    Aula 5: Criando o game loop no jogo da forca

--- forca.py
+++ forca.py
@@ -5,4 +5,16 @@ def jogar():
     print("O jogo de advinhação começou!!!")
     print("###############################")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    #Enquanto não enforcou e não acertou
+    while (not enforcou and not acertou):
+        print("jogando...")
+
+
     print("Fim do jogo!!")
+
+if (__name__ == __main__):
+    jogar()
